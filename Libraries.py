b3167c7e78ba720ac26c96172d0bd96a43b097bf
Masimthembe <mndabeni6@gmail.com>
2021-05-26 10:15:28 +0200

    first commit

---
+++ Libraries.py
@@ -0,0 +1,40 @@
+# exercise 1
+from datetime import datetime, timedelta
+dt = datetime(2021, 5, 26)
+# add two weeks from current date
+add_weeks = dt + timedelta(days=14)
+print(add_weeks.strftime("%Y-%m-%d"))
+
+add_weeks = dt + timedelta(days=28)
+print(add_weeks.strftime("%Y-%m-%d"))
+
+add_weeks = dt + timedelta(days=42)
+print(add_weeks.strftime("%Y-%m-%d"))
+
+add_weeks = dt + timedelta(days=56)
+print(add_weeks.strftime("%Y-%m-%d"))
+
+add_weeks = dt + timedelta(days=70)
+print(add_weeks.strftime("%Y-%m-%d"))
+
+add_weeks = dt + timedelta(days=84)
+print(add_weeks.strftime("%Y-%m-%d"))
+
+add_weeks = dt + timedelta(days=98)
+print(add_weeks.strftime("%Y-%m-%d"))
+
+add_weeks = dt + timedelta(days=112)
+print(add_weeks.strftime("%Y-%m-%d"))
+
+add_weeks = dt + timedelta(days=126)
+print(add_weeks.strftime("%Y-%m-%d"))
+
+add_weeks = dt + timedelta(days=140)
+print(add_weeks.strftime("%Y-%m-%d"))
+
+
+
+
+
+
+
